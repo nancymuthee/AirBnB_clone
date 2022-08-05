726251808848904e57b63799703a0833f8d97ead
wendy munyasi <wendymunyasi@gmail.com>
2022-08-05 22:29:36 +0300

    Update __init__method & add test for dict - task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 class BaseModel:
     """ Class that defines properties of base """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Creates new instances of Base """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for (key, value) in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """Returns a string represation of class details.
