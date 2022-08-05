c21b1f0b375354c19d371c4362d7a852831a9a88
wendy munyasi <wendymunyasi@gmail.com>
2022-08-05 21:53:02 +0300

    Update base model - task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,43 @@
+#!/usr/bin/python3
+"""Defines a class Base"""
 import uuid
-import datetime
-import string
-
+from datetime import datetime
 
 
 class BaseModel:
-    models = []
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
-    
+    """ Class that defines properties of base """
+
+    def __init__(self):
+        """ Creates new instances of Base """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
     def __str__(self):
-        return f'BaseModel({self.id}, {self.__dict__})'
-        
-    def save_model(self):
+        """Returns a string represation of class details.
+
+        Returns:
+            str: class details
+        """
+        string = "["
+        string += str(self.__class__.__name__) + '] ('
+        string += str(self.id) + ') ' + str(self.__dict__)
+        return string
+
+    def save(self):
+        """Update public instance attribute updated_at with current datetime.
+        """
         self.updated_at = datetime.datetime.now()
-        self.created_at = self.created_at
-        BaseModel.models.append(self)
-        
+
     def to_dict(self):
-        model_dict = {"id":self.id, "time created":self.created_at.isoformat(), "time updated":self.updated_at.isoformat()}
-        
-        return model_dict
+        """Returns a dictionary containing all key/values of __dict__ of
+        the instance.
+
+        Returns:
+            dict: key/value pairs.
+        """
+        dict_ = self.__dict__.copy()
+        dict_['__class__'] = self.__class__.__name__
+        dict_['created_at'] = self.created_at.isoformat()
+        dict_['updated_at'] = self.updated_at.isoformat()
+        return dict_
