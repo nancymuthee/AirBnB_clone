7f32d7e2d360caeb4e2b3efc34f491adb1adfc93
wendy munyasi <wendymunyasi@gmail.com>
2022-08-06 00:21:19 +0300

    Add create command in console.py

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ an empty line + ENTER shouldn’t execute anything.
 Your code should not be executed when imported
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -37,6 +38,23 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
+    def create(self, class_name):
+        """Creates a new instance of BaseModel, saves it (to the JSON
+        file) and prints the id.
+
+        Args:
+            class_name (class): name of current class.
+        """
+        classes = ['BaseModel']
+
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in classes:
+            print("** class doesn't exist **")
+        else:
+            new_model = BaseModel()
+            print(new_model.id)
+            new_model.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
