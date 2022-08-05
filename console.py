b13c2f74af6fd848872c21d8bd037668d20ae5dd
wendy munyasi <wendymunyasi@gmail.com>
2022-08-05 23:56:54 +0300

    Add console.py containing entry pnt of cmd intrptr

---
+++ console.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Contains the entry point of the command interpreter.
+
+You must use the module cmd.
+Your class definition must be: class HBNBCommand(cmd.Cmd):
+Your command interpreter should implement:
+quit and EOF to exit the program,
+help (this action is provided by default by cmd but you should keep it
+updated and documented as you work through tasks),
+a custom prompt: (hbnb),
+an empty line + ENTER shouldn’t execute anything.
+Your code should not be executed when imported
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class for command processor.
+
+    Args:
+        cmd (_type_): _description_
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, args):
+        """EOF command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """Empty line shouldn't execute anything
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
