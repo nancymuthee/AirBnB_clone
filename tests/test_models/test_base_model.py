cf46a46b6874b343e1989940df468b71f677ee4d
wendy munyasi <wendymunyasi@gmail.com>
2022-08-07 17:41:32 +0300

    Add tests for base_model and __init__.py files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,67 @@
 #!/usr/bin/python3
+""" Defines a class TestBaseModel for BaseModel module. """
+import unittest
 from models.base_model import BaseModel
+import datetime
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    """Defines tests for Amenity Class"""
+
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.BaseModel1 = BaseModel()
+        cls.BaseModel1.name = "Samsung"
+        cls.BaseModel1.my_number = 89
+
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.BaseModel1
+
+    def test_class_exists(self):
+        """Tests if class exists.
+        """
+        result = "<class 'models.base_model.BaseModel'>"
+        self.assertEqual(str(type(self.BaseModel1)), result)
+
+    def test_types(self):
+        """Test if attributes type is correct.
+        """
+        self.assertIsInstance(self.BaseModel1.name, str)
+        self.assertEqual(type(self.BaseModel1.name), str)
+        self.assertIsInstance(self.BaseModel1.id, str)
+        self.assertEqual(type(self.BaseModel1.id), str)
+        self.assertIsInstance(self.BaseModel1.created_at, datetime.datetime)
+        self.assertIsInstance(self.BaseModel1.updated_at, datetime.datetime)
+
+    def test_save(self):
+        """Test if save method is working correctly after update.
+        """
+        self.BaseModel1.save()
+        self.assertNotEqual(self.BaseModel1.created_at, self.BaseModel1.updated_at)
+
+    def test_functions(self):
+        """Test if BaseModel moudule is documented.
+        """
+        self.assertIsNotNone(BaseModel.__doc__)
+
+    def test_has_attributes(self):
+        """Test if expected attributes exist.
+        """
+        self.assertTrue(hasattr(self.BaseModel1, 'name'))
+        self.assertTrue(hasattr(self.BaseModel1, 'id'))
+        self.assertTrue(hasattr(self.BaseModel1, 'created_at'))
+        self.assertTrue(hasattr(self.BaseModel1, 'updated_at'))
+        
+    def test_set_attributes(self):
+        """Test set attributes of BaseModel.
+        """
+        self.assertEqual(self.BaseModel1.name, "Samsung")
+        self.assertEqual(self.BaseModel1.my_number, 89)
+
+if __name__ == '__main__':
+    unittest.main()
