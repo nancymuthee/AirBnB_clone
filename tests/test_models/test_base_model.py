df38bd40b1919ecb850b8e3a992a4944b846162a
wendy munyasi <wendymunyasi@gmail.com>
2022-08-07 18:06:58 +0300

    Add test for to_dict method in BaseModel test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -74,5 +74,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(self.BaseModel1.name, "Samsung")
         self.assertEqual(self.BaseModel1.my_number, 89)
 
+    def test_to_dict(self):
+        """Test if to_dict method is working correctly.
+        """
+        my_model_json = self.BaseModel1.to_dict()
+        self.assertEqual(str, type(my_model_json['created_at']))
+        self.assertEqual(my_model_json['created_at'],
+                         self.BaseModel1.created_at.isoformat())
+        self.assertEqual(datetime.datetime, type(self.BaseModel1.created_at))
+        self.assertEqual(my_model_json['__class__'],
+                         self.BaseModel1.__class__.__name__)
+        self.assertEqual(my_model_json['id'], self.BaseModel1.id)
+
 if __name__ == '__main__':
     unittest.main()
